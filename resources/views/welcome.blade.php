627d48a75aee09681795b188fd275571dd267f5d
TraianAlex <victor_traian@yahoo.com>
2017-11-25 18:21:30 -0500

    div

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,30 +1,18 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ URL::to('blog/css/main.css') }}">
+@endsection
+
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+    <div class="col-md-8">
+        <div class="panel panel-default">
+            <div class="panel-heading">Welcome</div>
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
+            <div class="panel-body">
+                Your Application's Landing Page.
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="centered">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <ul>
-                @for($i = 0; $i < 5; $i++)
-                    @if($i % 2 === 0)
-                        <li>Iteration {{ $i + 1 }}</li>
-                    @endif
-                @endfor
-            </ul>
         </div>
     </div>
 </div>
